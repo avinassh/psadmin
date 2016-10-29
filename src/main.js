8dff32d37c27271384e3722d0dfccd4d51a75348
avi <hi@avi.im>
2016-10-29 19:59:18 +0530

    Add a note about clean URLs

--- src/main.js
+++ src/main.js
@@ -5,5 +5,7 @@ var Router = require('react-router');
 var routes = require('./routes');
 
 Router.run(routes, function (Handler) {
+// if you want clean URLs (or URLs without hashes, pass Router.HistoryLocation)
+// Router.run(routes, Router.HistoryLocation, function (Handler)
     React.render(<Handler />, document.getElementById('app'));
 });
